0f837dc61724ebd93410fb6f107e681be230506f
gabzon <gab.zambrano@gmail.com>
2016-05-17 22:54:42 +0200

    seconde commit

--- templates/head.php
+++ templates/head.php
@@ -3,4 +3,23 @@
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <?php wp_head(); ?>
+
+  <!-- Custom Fonts -->
+  <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+  <link href='http://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+  <link href='https://fonts.googleapis.com/css?family=Roboto:400,300,300italic,400italic,700,700italic,900,900italic,500italic,500,100italic,100' rel='stylesheet' type='text/css'>
+  <script src="https://use.fontawesome.com/e47f35dff3.js"></script>
+
+  <!-- Plugin CSS -->
+  <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/styles/animate.min.css" type="text/css">
+
+  <!-- Custom CSS -->
+  <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/styles/creative.css" type="text/css">
+
+  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+  <![endif]-->
 </head>
