e1d4ff0d18c9ae00bd89594775350903e344c915
gabzon <gab.zambrano@gmail.com>
2016-05-19 01:56:50 +0200

    nav.php added to functions.php

--- functions.php
+++ functions.php
@@ -13,6 +13,7 @@ $sage_includes = [
   'lib/assets.php',    // Scripts and stylesheets
   'lib/extras.php',    // Custom functions
   'lib/setup.php',     // Theme setup
+  'lib/nav.php'        // navigation
   'lib/titles.php',    // Page titles
   'lib/wrapper.php',   // Theme wrapper class
   'lib/customizer.php' // Theme customizer
