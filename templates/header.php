05ed89c61d3a2d1ccde7ce6b2b707242a46560d1
gabzon <gab.zambrano@gmail.com>
2016-05-19 02:33:31 +0200

    header debugged

--- templates/header.php
+++ templates/header.php
@@ -4,24 +4,24 @@ use Roots\Sage\Nav\NavWalker;
 $menu_name = 'primary_navigation';
 ?>
 
-<header class="banner navbar navbar-fixed-top" role="banner">
-  <div class="container">
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
-        <span class="sr-only"><?= __('Toggle navigation', 'sage'); ?></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="<?= esc_url(home_url('/')); ?>"><img src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/logo-experiensa.png" alt="Dancefloor logo" class="ui small image" /></a>
-    </div>
+<header class="banner navbar navbar-fixed-top" role="banner" style="background-color:white;border-bottom:1px solid #eee">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+                <span class="sr-only"><?= __('Toggle navigation', 'sage'); ?></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="<?= esc_url(home_url('/')); ?>"><img src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/logo-experiensa.png" alt="Dancefloor logo" class="ui small image" /></a>
+        </div>
 
-    <nav class="collapse navbar-collapse" role="navigation" style="text-transform:uppercase">
-      <?php
-      if (has_nav_menu('primary_navigation')) :
-        wp_nav_menu(['theme_location' => 'primary_navigation', 'walker' => new NavWalker(), 'menu_class' => 'nav navbar-nav navbar-right']);
-      endif;
-      ?>
-    </nav>
-  </div>
+        <nav class="collapse navbar-collapse" role="navigation" style="text-transform:uppercase">
+            <?php
+            if (has_nav_menu('primary_navigation')) :
+                wp_nav_menu(['theme_location' => 'primary_navigation', 'walker' => new NavWalker(), 'menu_class' => 'nav navbar-nav navbar-right']);
+            endif;
+            ?>
+        </nav>
+    </div>
 </header>
